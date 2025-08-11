41a70e2fa2dc53794fa076bd725bfbad4d6a1bcc
gaetanbrison <gaetan.brison@gmail.com>
2025-08-11 12:27:30 +0200

    short path ⚙️

--- tarte_ai/__init__.py
+++ tarte_ai/__init__.py
@@ -9,3 +9,9 @@ from tarte_ai.tarte_preprocess_kg import *
 from tarte_ai.tarte_preprocess_table import *
 from tarte_ai.tarte_pretrain import *
 from tarte_ai.tarte_utils import *
+
+
+from tarte_ai.tarte_utils import load_data
+from tarte_ai.tarte_preprocess_table import TARTE_TablePreprocessor, TARTE_TableEncoder
+from tarte_ai.tarte_finetune_estimator import TARTEFinetuneRegressor, TARTEFinetuneClassifier, TARTEMultitableRegressor, TARTEMultitableClassifer
+from tarte_ai.tarte_boost_tabpfn import TARTEBoostRegressor_TabPFN, TARTEBoostClassifier_TabPFN, TARTEBaggingRegressor_TabPFN, TARTEBaggingClassifier_TabPFN
